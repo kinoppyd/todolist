3fcbe90b133f5863c15f4b88c9b9321595b26d83
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2014-06-22 18:02:36 +0900

    Tutorial: Formの宣言
    Formオブジェクトは、バリデーションを含んだHTMLフォームの定義をカプセル化する
    フォームをApplicationコントローラ内に作成する（ちょっとチュートリアルのサンプルコードがよくわからん）

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,12 +2,18 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
 
   def index = Action {
     Redirect(routes.Application.tasks)
   }
+
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
 
   def tasks = TODO
 
