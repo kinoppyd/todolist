8eba3847072649f5d5da283e55fd6c3b9e8ab1e3
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2014-06-22 18:50:02 +0900

    Tutorial: select, insert, deleteの処理をそれぞれ実装
    'hoge -> hoge っていう書き方は、どうもプレースホルダらしいがよくわからん

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -1,5 +1,8 @@
 package models
 
+import play.api.db._
+import play.api.Play.current
+
 import anorm._
 import anorm.SqlParser._
 
@@ -14,9 +17,23 @@ object Task {
     }
   }
 
-  def all(): List[Task] = Nil
+  def all(): List[Task] = DB.withConnection { implicit c =>
+    SQL("select * from task").as(task *)
+  }
 
-  def create(label: String) {}
+  def create(label: String) {
+    DB.withConnection { implicit c =>
+      SQL("insert into task (label) values ({label})").on(
+        'label -> label
+      ).executeUpdate()
+    }
+  }
 
-  def delete(id: Long) {}
+  def delete(id: Long) {
+    DB.withConnection { implicit c =>
+      SQL("delete from task where id = {id}").on(
+        'id -> id
+      ).executeUpdate()
+    }
+  }
 }
