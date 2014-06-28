4d97d83a0f903b4dc4da8362b0f6a3408bb4ec06
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2014-06-28 19:28:50 +0900

    EvolutionのクエリをMySQL用に変更

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,12 +2,10 @@
 
 # --- !Ups
 
-CREATE SEQUENCE task_id_seq;
 CREATE TABLE task (
-    id integer NOT NULL DEFAULT nextval('task_id_seq'),
+    id integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
     label varchar(255)
 );
 
 # --- !Downs
 DROP TABLE task;
-DROP SEQUENCE task_id_seq;
